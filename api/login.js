a9d1dbe40b3d589639755d77b2a25af43732e185
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-07-22 11:36:33 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ success: false, message: 'Faltan credenciales' });
     }
 
-    const url = `https://script.google.com/macros/s/AKfycbw7UcNdc0_jNQ0QL7gfsnAY6h4JzFEPAcIVReNLxeL33aM00qdoaeYIqVdtEDwMDlef/exec?email=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(contrasena)}`;
+    const url = `https://script.google.com/macros/s/AKfycbzgdYxYpAFrdVA4IkhnodLB9CKqwo-6Vbolv4PpnJRsjGroWbO409rtxXIDajF8Rppl/exec?email=${encodeURIComponent(email)}&contrasena=${encodeURIComponent(contrasena)}`;
 
  const response = await fetch(url);
     const text = await response.text();
