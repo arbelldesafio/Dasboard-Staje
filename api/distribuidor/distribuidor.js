de404bbbfc309d4edfd3f37ffad4c60c00181566
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-07-22 13:55:10 -0300

    Update distribuidor.js

--- api/distribuidor/distribuidor.js
+++ api/distribuidor/distribuidor.js
@@ -1,17 +1,1 @@
-export default async function handler(req, res) {
-  const { distribuidor } = req.query;
 
-  if (!distribuidor) {
-    return res.status(400).json({ success: false, message: "Falta el parámetro 'distribuidor'" });
-  }
-
-  try {
-    const response = await fetch(`https://script.google.com/macros/s/AKfycbzkOF8vOphZKysealkkNiVoLkjLYRy2jxx10piLYDyY7B2VznC8Gfa9bwcbzK4R_IvX/exec?distribuidor=${encodeURIComponent(distribuidor)}`);
-    const data = await response.json();
-
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error("Error al consultar distribuidor:", error);
-    return res.status(500).json({ success: false, message: "Error en el servidor" });
-  }
-}
